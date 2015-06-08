6a2d2d487fa85ef5c5cd37fa691e7d4abcf6f725
jay <jaybharatjay@gmail.com>
2015-06-09 00:04:42 +0530

    Changes in plugin code
    Changes in plugin code

--- wordpress/wp-content/plugins/jayplugin2/index1.php
+++ wordpress/wp-content/plugins/jayplugin2/index1.php
@@ -9,56 +9,17 @@ Author URI: http://www.bharatbaba.com
 */
 
 function jay_comments($howmany) {
-	//echo "how=".$howmany;
-    //echo 'from plugin';
-  //$wpdb->query('select * from my_plugin_table where foo = "bar"');
+	
   global $wpdb;
   $request = "SELECT ID, comment_ID, comment_content, comment_author, comment_author_url, post_title FROM $wpdb->comments LEFT JOIN $wpdb->posts ON $wpdb->posts.ID=$wpdb->comments.comment_post_ID WHERE post_status IN ('publish','static') ";
-	//if(!$show_pass_post) $request .= "AND post_password ='' ";
-	//$request .= "AND comment_approved = '1' ORDER BY comment_ID DESC LIMIT $no_comments";
 	$comments = $wpdb->get_results($request);
 	echo "<pre>";print_r($comments);echo "</pre>";
 }
 
 function jay_posts($howmany) {
 
-
-//create table start
-//function jal_install () {
-   //global $wpdb;
-
-   //$table_name = $wpdb->prefix . "liveshoutbox"; 
-//}
-
-
-
-global $wpdb;
-
-$charset_collate = $wpdb->get_charset_collate();
-
-$sql = "CREATE TABLE $table_name (
-  id mediumint(9) NOT NULL AUTO_INCREMENT,
-  time datetime DEFAULT '0000-00-00 00:00:00' NOT NULL,
-  name tinytext NOT NULL,
-  text text NOT NULL,
-  url varchar(55) DEFAULT '' NOT NULL,
-  UNIQUE KEY id (id)
-) $charset_collate;";
-
-require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
-dbDelta( $sql );
-echo "I am here";
-//create table close
-
-
-
-
-
-
-
-
 	  global $wpdb;
-      $query = "SELECT * FROM $wpdb->posts
+    $query = "SELECT * FROM $wpdb->posts
 LEFT JOIN $wpdb->term_relationships ON
 ($wpdb->posts.ID = $wpdb->term_relationships.object_id)
 LEFT JOIN $wpdb->term_taxonomy ON
@@ -66,7 +27,7 @@ LEFT JOIN $wpdb->term_taxonomy ON
 WHERE $wpdb->posts.post_status = 'publish'
 AND $wpdb->term_taxonomy.taxonomy = 'category'
 AND $wpdb->term_taxonomy.term_id = 1
-ORDER BY post_date DESC
+ORDER BY post_date ASC
 ";
 
 	$results = $wpdb->get_results($query);
@@ -75,7 +36,7 @@ ORDER BY post_date DESC
 	//class/array data to object
 	foreach($results as $temp){
 		echo "<hr>";
-		echo "<br>ID=".$temp->ID;
+		echo "<br>ID=".$ID = $temp->ID;
 		echo "<br>post_author=".$post_author = $temp->post_author;
 		echo "<br>post_date=".$post_date = $temp->post_date;
 		echo "<br>post_date_gmt=".$post_date_gmt = $temp->post_date_gmt;
@@ -106,14 +67,100 @@ ORDER BY post_date DESC
 		echo "<br>description=".$description = $temp->description;
 		echo "<br>parent=".$parent = $temp->parent;
 		echo "<br>count=".$count = $temp->count;
+
+
+    //checking the duplicate record in db start
+    //global $wpdb;
+    echo $query = "SELECT COUNT(id) AS total FROM viz_posts
+WHERE posts_ID = '".$ID."' LIMIT 0,1
+";
+  $results = $wpdb->get_results($query);
+  echo "<br>Total duplicate=";
+  $duplicate = 0;
+  echo $duplicate = $results[0]->total;//print_r($results);
+  //checking the duplicate record in db close
+
+
+
+    //post saving into db table start
+  if($duplicate){
+    //update
+      /*global $wpdb;
+      $foobarOptie = $_POST["foobar"];
+      $deleteOptie = $_POST["DeleteData"];
+      $table = "wp_cool_plugin_options";
+      $data_array = array('idOptions' => 1, 'isFoobar' => $foobarOptie, 'DeleteData' => $deleteOptie);
+      $where = array('idOptions' => 1);
+      $wpdb->update( $table, $data_array, $where );*/
+
+      global $wpdb;
+      $table = "viz_posts";
+      $data_array = array(
+        'post_author' => $post_author, 'post_date' => $post_date, 'post_date_gmt' => $post_date_gmt, 
+        'post_content' => $post_content, 'post_title' => $post_title, 'post_excerpt' => $post_excerpt, 
+        'post_status' => $post_status, 'comment_status' => $comment_status, 'ping_status' => $ping_status,
+        'post_password' => $post_password, 'post_name' => $post_name, 'to_ping' => $to_ping
+        , 'pinged' => $pinged, 'post_modified' => $post_modified, 'post_modified_gmt' => $post_modified_gmt
+        , 'post_content_filtered' => $post_content_filtered, 'post_parent' => $post_parent, 'guid' => $guid
+        , 'menu_order' => $menu_order, 'post_type' => $post_type, 'post_mime_type' => $post_mime_type
+        , 'comment_count' => $comment_count, 'object_id' => $object_id, 'term_taxonomy_id' => $term_taxonomy_id
+        , 'term_order' => $term_order, 'term_id' => $term_id, 'taxonomy' => $taxonomy
+        , 'description' => $description, 'parent' => $parent, 'count' => $count
+       );
+      $where = array('posts_ID' => $ID);
+      $wpdb->update( $table, $data_array, $where );
+
+  }else{
+    //insert
+    global $wpdb;
+    $wpdb->insert("viz_posts", array(
+     "posts_ID" => $ID,
+     "post_author" => $post_author,
+     "post_date" => $post_date,
+     "post_date_gmt" => $post_date_gmt,
+     "post_content" => $post_content,
+     "post_title" => $post_title,
+     "post_excerpt" => $post_excerpt,
+     "post_status" => $post_status ,
+     "comment_status" => $comment_status ,
+     "ping_status" => $ping_status ,
+     "post_password" => $post_password ,
+     "post_name" => $post_name ,
+     "to_ping" => $to_ping ,
+     "pinged" => $pinged ,
+     "post_modified" => $post_modified ,
+     "post_modified_gmt" => $post_modified_gmt ,
+     "post_content_filtered" => $post_content_filtered ,
+     "post_date_gmt" => $post_parent ,
+     "guid" => $guid ,
+     "menu_order" => $menu_order ,
+     "post_type" => $post_type ,
+     "post_mime_type" => $post_mime_type ,
+     "comment_count" => $comment_count ,
+     "object_id" => $object_id ,
+     "term_taxonomy_id" => $term_taxonomy_id ,
+     "term_order" => $term_order ,
+     "term_id" => $term_id ,
+     "taxonomy" => $taxonomy ,
+     "description" => $description ,
+     "parent" => $parent ,
+     "count" => $count 
+  ));
+}
+//post saving into db table close
 		
-	}
-	return $results;
+	}//foreach close
+	//return $results;
+
+
+
+
 	/*
 	//ID,post_author,post_date,post_date_gmt,post_content,post_title,post_excerpt,post_status,comment_status,ping_status,post_password,post_name,to_ping,pinged,post_modified,post_modified_gmt,post_content_filtered,post_parent,guid,menu_order,post_type,post_mime_type,comment_count,object_id,term_taxonomy_id,term_order,term_id,taxonomy,description,parent,count
 	
 	CREATE TABLE `viz_posts` (
-  `ID` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT 'PK',
+  `posts_ID` bigint(20) unsigned NOT NULL COMMENT 'FK',
   `post_author` bigint(20) unsigned NOT NULL DEFAULT '0',
   `post_date` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
   `post_date_gmt` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
@@ -144,13 +191,15 @@ ORDER BY post_date DESC
   `description` longtext NOT NULL COMMENT 'wp_term_taxonomy',
   `parent` bigint(20) unsigned NOT NULL DEFAULT '0' COMMENT 'wp_term_taxonomy',
   `count` bigint(20) NOT NULL DEFAULT '0' COMMENT 'wp_term_taxonomy',
-  PRIMARY KEY (`ID`),
+  PRIMARY KEY (`id`),
   KEY `post_name` (`post_name`(191)),
-  KEY `type_status_date` (`post_type`,`post_status`,`post_date`,`ID`),
+  KEY `type_status_date` (`post_type`,`post_status`,`post_date`,`posts_ID`),
   KEY `post_parent` (`post_parent`),
   KEY `post_author` (`post_author`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 */
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
-}
+*/
+
+}//function close
 
 //code closed
