74dfe9497c0a312d34a021a44d97e34f2850d4b2
jay <jaybharatjay@gmail.com>
2015-08-31 01:44:23 +0530

    committing import post
    committing import post

--- wordpress/wp-content/plugins/importPosts/index.php
+++ wordpress/wp-content/plugins/importPosts/index.php
@@ -7,17 +7,137 @@ Author: Jay Bharat/9844542127/jaybharatjay@gmail.com
 Version: 5.0/31-July-2015
 Author URI: http://www.bharatbaba.com
 */
-
+//new post or edit post
 
 $importPosts = new importPostsComments();
 class importPostsComments{
 		public function __construct(){
-			//Hook for adding importPostsComments
-			add_action('init', array($this, 'importPosts'), 0);
-			add_action('init', array($this, 'importComments'), 0); 
-			add_action('init', array($this, 'importSentiment'), 0); 
-			
+			if(count($_POST)){
+			    $post_ID = '';
+    			if(@$_POST['_wp_http_referer']){
+        			if(strripos($_POST['_wp_http_referer'],"post-new")){
+        			    //add now in db
+            			add_action('edit_post', array($this, 'addpostCumPubnubPost'), 0);
+        			}
+        			else if(strripos($_POST['_wp_http_referer'],"edit")){
+        			    //edit now in db
+        			    $post_ID = $_POST['post_ID'];
+        			    add_action('edit_post', array($this, 'editpostCumPubnubPost'), 0);
+        			}
+    			}
+    			else if(@$_POST['comment_post_ID'] != ""){
+    			    //comment triggered
+                    add_action('comment_post', array($this, 'giveMeCommentId'), 0);
+                }
+			}//post close
+			else if(@$_GET['action'] === "trash"){
+			    add_action('edit_post', array($this, 'deletepostCumPubnubPost'), 0);  
+            }
 		}
+		
+        public function giveMeCommentId() {
+             $this->dbTableComments();
+             $comment_post_ID = $_POST['comment_post_ID'];
+             $comment = $_POST['comment'];
+             global $wpdb;
+             $query = "SELECT comment_ID  
+			FROM $wpdb->comments 
+			WHERE comment_post_ID = '$comment_post_ID'
+			AND 
+            comment_content  = '$comment'
+			ORDER BY comment_ID DESC
+			LIMIT 0,1
+		    ";
+			$results = $wpdb->get_results($query,OBJECT);
+			$comment_ID = $results[0]->comment_ID;
+			$trueFalse = $this->editCommentsCumPubnunCommentPublish($comment_ID);
+			$trueFalse = $this->importSentiment($comment_ID);
+			$this->pubnubPublishComments($comment_ID);
+        }
+		
+        
+        public function addpostCumPubnubPost($post_ID){
+            $this->dbTablePosts();
+            $results = $this->getWpPosts($post_ID);            
+            if($this->ifDuplicatePost($post_ID)){
+                $this->updatePost($results[0]);
+            }else{
+                $this->insertPost($results[0]);
+            }
+            $this->pubnubPostNewPublish($post_ID);    
+        }
+		
+		
+		
+		public function deletepostCumPubnubPost($post_ID){
+            $this->deletePost($post_ID);           
+            $this->pubnubPostDeletePublish($post_ID);    
+        }
+		
+		
+		public function editpostCumPubnubPost() {
+		    $this->dbTablePosts();
+            $post_ID = $_POST['post_ID'];
+            $results = $this->getWpPosts($post_ID);            
+            if($this->ifDuplicatePost($post_ID)){
+                $this->updatePost($results[0]);
+            }else{
+                $this->insertPost($results[0]);
+            }
+            $this->pubnubPostEditPublish($post_ID);
+        }
+        
+        private function updatePost($temp){
+		     $ID = $temp->ID;
+		     $post_author = $temp->post_author;
+		     $post_date = $temp->post_date;
+		     $post_date_gmt = $temp->post_date_gmt;
+		     $post_content = $temp->post_content;
+		     $post_title = $temp->post_title;
+		     $post_status = $temp->post_status;
+		     $post_name = $temp->post_name;
+		     $post_modified = $temp->post_modified;
+		     $post_modified_gmt = $temp->post_modified_gmt;
+		     $post_type = $temp->post_type;
+		     $comment_count = $temp->comment_count;
+		     //row data close
+		      //update
+		      global $wpdb;
+		      $table = "viz_posts";
+		      $data_array = array(
+		        'post_author' => $post_author, 
+		        'post_date' => $post_date, 
+		        'post_date_gmt' => $post_date_gmt, 
+		        'post_content' => $post_content, 
+		        'post_title' => $post_title, 
+		        'post_status' => $post_status,
+		        'post_name' => $post_name,
+		        'post_modified' => $post_modified, 
+		        'post_modified_gmt' => $post_modified_gmt,
+		        'post_type' => $post_type,
+		        'comment_count' => $comment_count
+		        
+		       );
+		      $where = array('posts_ID' => $ID);
+		      $wpdb->update( $table, $data_array, $where );
+		      //update close
+		    //categories start
+            $this->dbTableCategories();
+		    if(count($temp)){
+                    $ID = $temp->ID;
+					$resultsCategories = $this->categoriesForPost($ID);
+					$this->updateCategories($resultsCategories,$ID);
+		    }
+            //categories close
+            return true;
+		}
+		
+		private function deletePost($post_ID){
+		    global $wpdb;
+    		$wpdb->delete( 'viz_posts', array( 'posts_ID' => $post_ID ) );
+    		return true;
+		}
+		
 		private function dbTablePosts(){
 			//table create start
 		    global $wpdb;
@@ -45,30 +165,25 @@ class importPostsComments{
 		  ) ENGINE=MyISAM $charset_collate AUTO_INCREMENT=1;";
 		  require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
 		  dbDelta( $sql );
-
-
-
-		  
-
-
 		  //table create close
-		}//function dbTablePosts close
+		}
 		
-		
-
-		private function getWpPosts(){
+		private function getWpPosts($post_ID){
 			global $wpdb;
-			$query = "SELECT $wpdb->posts.ID,$wpdb->posts.post_author,$wpdb->posts.post_date,$wpdb->posts.post_date_gmt,$wpdb->posts.post_content,$wpdb->posts.post_title,$wpdb->posts.post_status,$wpdb->posts.post_name,$wpdb->posts.post_modified,$wpdb->posts.post_modified_gmt,$wpdb->posts.post_type,$wpdb->posts.comment_count FROM $wpdb->posts
+            $query = "SELECT $wpdb->posts.ID,$wpdb->posts.post_author,$wpdb->posts.post_date,$wpdb->posts.post_date_gmt,$wpdb->posts.post_content,$wpdb->posts.post_title,$wpdb->posts.post_status,$wpdb->posts.post_name,$wpdb->posts.post_modified,$wpdb->posts.post_modified_gmt,$wpdb->posts.post_type,$wpdb->posts.comment_count FROM $wpdb->posts
 		    
 		    WHERE $wpdb->posts.post_status = 'publish'
 		    AND $wpdb->posts.post_type = 'post'
-		    ORDER BY $wpdb->posts.post_date ASC
+		    AND $wpdb->posts.ID = $post_ID
+		    LIMIT 0,1
 		    ";
 		   
 			$results = $wpdb->get_results($query);
 			//print_r($results);
 			return $results;
 		}
+
+		
 		private function ifDuplicatePost($ID){
 			//checking the duplicate record in db start
 		     global $wpdb;
@@ -82,44 +197,9 @@ class importPostsComments{
 		     //checking the duplicate record in db close
 		
 		}
-		private function updatePost($temp){
-			 //row data start, class/array data to object
-		     $ID = $temp->ID;
-		     $post_author = $temp->post_author;
-		     $post_date = $temp->post_date;
-		     $post_date_gmt = $temp->post_date_gmt;
-		     $post_content = $temp->post_content;
-		     $post_title = $temp->post_title;
-		     $post_status = $temp->post_status;
-		     $post_name = $temp->post_name;
-		     $post_modified = $temp->post_modified;
-		     $post_modified_gmt = $temp->post_modified_gmt;
-		     $post_type = $temp->post_type;
-		     $comment_count = $temp->comment_count;
-		     
-		     //row data close
-		      //update
-		      global $wpdb;
-		      $table = "viz_posts";
-		      $data_array = array(
-		        'post_author' => $post_author, 
-		        'post_date' => $post_date, 
-		        'post_date_gmt' => $post_date_gmt, 
-		        'post_content' => $post_content, 
-		        'post_title' => $post_title, 
-		        'post_status' => $post_status,
-		        'post_name' => $post_name,
-		        'post_modified' => $post_modified, 
-		        'post_modified_gmt' => $post_modified_gmt,
-		        'post_type' => $post_type,
-		        'comment_count' => $comment_count
-		        
-		       );
-		      $where = array('posts_ID' => $ID);
-		      $wpdb->update( $table, $data_array, $where );
-		      return true;
-		      //update close
-		}//function update close
+		
+		
+		
 		private function insertPost($temp){
 			 //row data start, class/array data to object
 		     $ID = $temp->ID;
@@ -153,36 +233,19 @@ class importPostsComments{
 		     "comment_count" => $comment_count 
 		    
 		     ));
-		     return true;
-		}//function insert close
-		
-		public function importPosts($howmany) {
-		    $this->dbTablePosts();
-			$results = $this->getWpPosts();
-			if(count($results)){
-			 for($i = 0; $i < count($results); $i++){
-		      $ID = $results[$i]->ID;
-		      $duplicate = $this->ifDuplicatePost($ID);
-		      if($duplicate){
-		       $this->updatePost($results[$i]);
-		      }else{
-		       $this->insertPost($results[$i]);
-		      }
-		     }//for close
-		    }//if close 
-
-		    $this->dbTableCategories();
-		    if(count($results)){
-		    	for($i = 0; $i < count($results); $i++){
-		    	    $ID = $results[$i]->ID;
-
+		     
+		     //categories start
+            $this->dbTableCategories();
+		    if(count($temp)){
+                    $ID = $temp->ID;
 					$resultsCategories = $this->categoriesForPost($ID);
 					$this->updateCategories($resultsCategories,$ID);
-
-		      	}
 		    }
-		}//function importPosts close
-
+            //categories close
+            
+		     return true;
+		}
+		
 		private function dbTableComments(){
 			//table create start
 		    global $wpdb;
@@ -203,12 +266,11 @@ class importPostsComments{
 		  require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
 		  dbDelta( $sql );
 		  //table create close
-		}//function dbTable close
+		}
 
-		private function getWpComments(){
+		private function getWpComments($comment_id){
 			global $wpdb;
-			$query = "SELECT comment_ID, comment_post_ID,  comment_content, comment_author, comment_author_email, comment_date,comment_date_gmt, comment_approved FROM $wpdb->comments ORDER BY  comment_id ASC
-		    ";
+             $query = "SELECT comment_ID, comment_post_ID,  comment_content, comment_author, comment_author_email, comment_date,comment_date_gmt, comment_approved FROM $wpdb->comments WHERE  comment_id='$comment_id'";
 		   	$results = $wpdb->get_results($query);
 			return $results;
 		}
@@ -216,7 +278,7 @@ class importPostsComments{
 		private function ifDuplicateComments($comment_ID){
 			//checking the duplicate record in db start
 		     global $wpdb;
-		     $query = "SELECT COUNT(id) AS total FROM viz_comments
+		      $query = "SELECT COUNT(id) AS total FROM viz_comments
 		     WHERE comment_id = '".$comment_ID."' LIMIT 0,1
 		     ";
 		     $results = $wpdb->get_results($query);
@@ -224,7 +286,6 @@ class importPostsComments{
 		     $duplicate = $results[0]->total;
 		     return $duplicate;
 		     //checking the duplicate record in db close
-		
 		}
 
 		private function updateComments($temp){
@@ -253,24 +314,26 @@ class importPostsComments{
 		       );
 		      $where = array('comment_id' => $comment_ID);
 		      $wpdb->update( $table, $data_array, $where );
+		      echo "<pre>";print_r($data_array);echo "</pre>";
 		      return true;
 		      //update close
-		}//function update close
+		}
 
 
 		private function insertComments($temp){
 			 //row data start, class/array data to object
-		     $comment_ID = $temp->comment_ID;
-		     $comment_post_ID = $temp->comment_post_ID;
-		     $comment_content = $temp->comment_content;
-		     $comment_author = $temp->comment_author;
-		     $comment_author_email = $temp->comment_author_email;
-		     $comment_date = $temp->comment_date;
-		     $comment_date_gmt = $temp->comment_date_gmt;
-		     $comment_approved = $temp->comment_approved;
+			 //print_r($temp);
+		     $comment_ID = $temp[0]->comment_ID;
+		     $comment_post_ID = $temp[0]->comment_post_ID;
+		     $comment_content = $temp[0]->comment_content;
+		     $comment_author = $temp[0]->comment_author;
+		     $comment_author_email = $temp[0]->comment_author_email;
+		     $comment_date = $temp[0]->comment_date;
+		     $comment_date_gmt = $temp[0]->comment_date_gmt;
+		     $comment_approved = $temp[0]->comment_approved;
 		     //row data close
 		    
-			global $wpdb;
+			 global $wpdb;
 		     $wpdb->insert("viz_comments", array(
 		     "comment_id" => $comment_ID,
 		     'posts_id' => $comment_post_ID, 
@@ -282,26 +345,19 @@ class importPostsComments{
 		     'comment_approved' => $comment_approved
 		     ));
 		     return true;
-		}//function insert close
+		}
 
-		public function importComments($howmany) {
-		    $this->dbTableComments();
-			$results = $this->getWpComments();
+		private function editCommentsCumPubnunCommentPublish($comment_id) {
+			$results = $this->getWpComments($comment_id);
 			if(count($results)){
-			 for($i = 0; $i < count($results); $i++){
-		      $comment_id = $results[$i]->comment_ID;
 		      $duplicate = $this->ifDuplicateComments($comment_id);
 		      if($duplicate){
-		       $this->updateComments($results[$i]);
+		       $this->updateComments($results);
 		      }else{
-		       $this->insertComments($results[$i]);
+		       $this->insertComments($results);
 		      }
-		     }//for close
 		    }//if close  
-		}//function importComments close
-
-
-
+		}
 
 		private function dbTableCategories(){
 			//table create start
@@ -312,21 +368,13 @@ class importPostsComments{
 		  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT 'PK',
 		  `term_id` bigint(20) unsigned NOT NULL COMMENT 'FK',
 		  `post_id` bigint(20) unsigned NOT NULL COMMENT 'FK',
-		  
 		  `name` varchar(200) NOT NULL DEFAULT '',
 		  `slug` varchar(200) NOT NULL DEFAULT '',  
 		  `term_group` bigint(10) unsigned NOT NULL COMMENT '',
-		  
 		  PRIMARY KEY (`id`)
-		  
 		  ) ENGINE=MyISAM $charset_collate AUTO_INCREMENT=1;";
-		  require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
-		  dbDelta( $sql );
-
-
-		
-
-
+		  require_once(ABSPATH . 'wp-admin/includes/upgrade.php' );
+		  dbDelta( $sql);
 		  //table create close
 		}//function dbTableCategories close
 		
@@ -337,40 +385,29 @@ class importPostsComments{
 		    $table_name='viz_sentiment';
 		   $sql = "CREATE TABLE IF NOT EXISTS $table_name (
 		  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT 'PK',
-		  
 		  `comment_id` bigint(20) unsigned NOT NULL COMMENT 'FK',
-		  
 		  `neg` varchar(256) NOT NULL DEFAULT '',
 		  `neutral` varchar(256) NOT NULL DEFAULT '',  
 		  `pos` varchar(256)  NOT NULL COMMENT '',
 		  `label` varchar(256)  NOT NULL COMMENT '',
-		  
 		  PRIMARY KEY (`id`)
-		  
 		  ) ENGINE=MyISAM $charset_collate AUTO_INCREMENT=1;";
 		  require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
 		  dbDelta( $sql );
-
-
-		
-
-
 		  //table create close
-		}//function dbTableCategories close
+		}//function dbTableSentiment close
 		
-		
-
 		private function categoriesForPost($posts_id){
 			global $wpdb;
 			$query = "SELECT wt.* FROM wp_posts p
- INNER JOIN wp_term_relationships r ON r.object_id=p.ID
- INNER JOIN wp_term_taxonomy t ON t.term_taxonomy_id = r.term_taxonomy_id
- INNER JOIN wp_terms wt on wt.term_id = t.term_id
-WHERE p.ID=$posts_id AND t.taxonomy='category'
+            INNER JOIN wp_term_relationships r ON r.object_id=p.ID
+            INNER JOIN wp_term_taxonomy t ON t.term_taxonomy_id = r.term_taxonomy_id
+            INNER JOIN wp_terms wt on wt.term_id = t.term_id
+            WHERE p.ID=$posts_id AND t.taxonomy='category'
 		    ";
 		   	$resultsCategories = $wpdb->get_results($query);
 			return $resultsCategories;
-		}//function categoriesForPost close
+		}
 
 		private function updateCategories($resultsCategories,$postID){
 			//delete present categories for crossponding postID
@@ -379,56 +416,41 @@ WHERE p.ID=$posts_id AND t.taxonomy='category'
 			global $wpdb;
 			$wpdb->query($qry);
 			//delete close
-
+            //echo "<pre>";print_r($resultsCategories);echo "</pre>";
 			foreach($resultsCategories as $temp){
 			     $term_id = $temp->term_id;
 			     $post_id = $postID;
 			     $name = $temp->name;
 			     $slug = $temp->slug;
 			     $term_group = $temp->term_group;
+			     $wpdb->insert($table_name, array(
+    		     "term_id" => $term_id,
+    		     "post_id" => $post_id,
+    		     "name" => $name,
+    		     "slug" => $slug,
+    		     "term_group" => $term_group
+    		     ));
 		    }//foreach close
-
-			//global $wpdb;
-		     $wpdb->insert($table_name, array(
-		     "term_id" => $term_id,
-		     "post_id" => $post_id,
-		     "name" => $name,
-		     "slug" => $slug,
-		     "term_group" => $term_group
-		     ));
-		     return true;
-		}//function categoriesForPost close
+            return true;
+		}
 		
-		public function importSentiment(){
-		
+		public function importSentiment($comment_ID){
 		    $this->dbTableSentiment();
-		
-    		$results = $this->getWpComments();
-    		//print_r($results);
-    		
+    		$results = $this->getWpComments($comment_ID);
     		if(count($results)){
 			 for($i = 0; $i < count($results); $i++){
-			  
-			    if($results[$i]->comment_approved=='1'){
+			    //if($results[$i]->comment_approved=='1')
+			    {
     		      $comment_content = $results[$i]->comment_content;
     		      $comment_ID = $results[$i]->comment_ID;
-    		      
-    		      
-    		      
     		      //curl start
         	        //$post = "language=english&text='super awesome movie'" ;
-        	      
         	        $post = "language=english&text=$comment_content" ;
-        
-        
                     $headers = array();
                     $headers[] = 'X-Mashape-Key: qMoASitCA6mshhjabj2hxCxq1iDYp1wZgKUjsnI2TgbAhvgJls';
                     $headers[] = 'Content-Type: application/x-www-form-urlencoded';
                     $headers[] = 'Accept: application/json';
-                    
-                    
                     $ch = curl_init();
-                    
                     curl_setopt($ch, CURLOPT_URL, 'https://japerk-text-processing.p.mashape.com/sentiment/');
                     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
                     curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
@@ -458,7 +480,7 @@ WHERE p.ID=$posts_id AND t.taxonomy='category'
                 }//if close $results[$i]->comment_approved
 		     }//for close
 		    }//if close  
-		}//function importSentiment close
+		}
 		
 		private function addEditSentiment($comment_ID,$neg,$neutral,$pos,$label){
     		global $wpdb;
@@ -499,8 +521,50 @@ WHERE p.ID=$posts_id AND t.taxonomy='category'
                  //add close
              }//else close
              return false;
-		}//function addEditSentiment close
+		}
+		
+		
+		private function pubnubPostEditPublish($whichPost) {
+		    if(@$whichPost){
+		        include( plugin_dir_path( __FILE__ ) . 'pubnubPostEditPublish.php');
+            }
+		   
+		}
+		
+		
+		
+		private function pubnubPostNewPublish($whichPost) {
+		    if(@$whichPost){
+		        include( plugin_dir_path( __FILE__ ) . 'pubnubPostNewPublish.php');
+            }
+		   
+		}
+        private function pubnubPostDeletePublish($whichPost) {
+		    if(@$whichPost){
+		        include( plugin_dir_path( __FILE__ ) . 'pubnubPostDeletePublish.php');
+            }
+		   
+		}
+		
+		
+		private function pubnubPublishComments($whichComment) {
+		    if(@$whichComment){
+		        include( plugin_dir_path( __FILE__ ) . 'publishComments.php');
+            }
+		   
+		}
+
+		
+
 		
 }//class close
 //code closed
+
+
+
+
+
+
+
+
 
---
+++ wordpress/wp-content/plugins/importPosts/pubnubPostNewPublish.php
@@ -0,0 +1,96 @@
+<?php
+//categories
+ini_set("display_errors", 1);
+require_once('lib/autoloader.php');
+use Pubnub\Pubnub;
+$pubnub = new Pubnub('pub-c-3e92490d-3935-49d6-a2f1-f7f935f88036', 'sub-c-6c450ff2-3ae9-11e5-8579-02ee2ddab7fe');
+$data = getPosts($pubnub,$whichPost);
+function getPosts($pubnub,$whichPost){
+    global $wpdb;  
+    $postsCommentsJson = '';
+    $queryAllPosts = "SELECT viz_posts.id,viz_posts.posts_ID,viz_posts.post_author,viz_posts.post_date,viz_posts.post_date_gmt,viz_posts.post_content,viz_posts.post_title,viz_posts.post_status,viz_posts.post_name,viz_posts.post_modified,viz_posts.post_modified_gmt,viz_posts.post_type,viz_posts.comment_count FROM viz_posts 
+    WHERE viz_posts.post_status = 'publish'
+    AND viz_posts.post_type = 'post'
+    AND  viz_posts.posts_ID=$whichPost
+    ORDER BY viz_posts.post_date ASC
+   
+    ";
+    $results = $wpdb->get_results($queryAllPosts);
+    if(count($results)>=1){
+       $data = '';
+        foreach($results as $temp){
+            $id = $temp->id;
+            $pid = $temp->posts_ID;
+            $post_author = $temp->post_author;
+            $post_date = $temp->post_date;
+            $post_date_gmt = $temp->post_date_gmt;
+            $post_content = $temp->post_content;
+            $post_title = $temp->post_title;
+            $post_status = $temp->post_status;
+            $post_name = $temp->post_name;
+            $post_modified = $temp->post_modified;
+            $post_modified_gmt = $temp->post_modified_gmt;
+            $post_type = $temp->post_type;
+            $comment_count = $temp->comment_count;
+            $post_content = cleanObject($post_content);
+            $post_content = addslashes($post_content);
+            $temp3='';
+            
+            $categories = getCategories($pid);
+            $temp3='';
+            if(count($categories)>=1){
+                
+                foreach($categories as $temp2){
+                    $temp3.=$temp2->name.',';
+                }
+                
+                $temp3=substr($temp3,0,-1);
+            }
+            
+            $data.="{".
+                '"id":'. '"'.$id.'",'.
+                '"pid":'. '"'.$pid.'",'.
+                '"post_author":'. '"'.$post_author.'",'.
+                '"post_date":'. '"'.$post_date.'",'.
+                '"post_content":'. '"'.$post_content.'",'.
+                '"post_title":'. '"'.$post_title.'",'.
+                '"post_name":'. '"'.$post_name.'",'.
+                '"categories":'. '"'.$temp3.'"';
+            
+            
+            
+            
+        }//foreach close
+        
+        //$data=substr($data,0,-1); 
+        
+        //$data='{'.'"result":'.'"Yes",'.'"records"'.':['.$data.'}]}';
+        $data='{'.'"result":'.'"Yes",'.'"action":'.'"New",'.'"records"'.':['.$data.'}]}';
+              
+        //die($data);
+        $publish_result = $pubnub->publish('demojay',$data);
+        
+    }else{
+        $output = json_encode(array('result'=>'errorCommon', 'text' => 'Norecords'));
+        die($output);
+    }
+}
+
+
+
+function cleanObject($object){
+    //$val = array("\n","\r");
+    $val = array("\r\n", "\n", "\r");
+    return $object = str_replace($val, "<br>", $object);
+}
+
+function getCategories($pid){
+    global $wpdb;
+    $query = "SELECT
+    name FROM viz_categories WHERE post_id='".$pid."'";
+    $results = $wpdb->get_results($query);
+    //print_r($results);//die('12');
+    return $results;
+
+}
+?>
